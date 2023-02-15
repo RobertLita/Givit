d37bb5533f7df63cfabdc64abb19b3900533460e
RobertLita <lita.robert@yahoo.com>
2023-02-15 17:24:20 +0200

    db connection added

---
+++ backend/app/core/config.py
@@ -0,0 +1,30 @@
+from pydantic import BaseSettings, PostgresDsn, validator
+from typing import Any
+
+
+class Settings(BaseSettings):
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DATABASE_URI: PostgresDsn | None = None
+    PROJECT_NAME: str
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: list[str] | None, values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
