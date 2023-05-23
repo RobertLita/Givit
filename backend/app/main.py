d09a0538294a6865ae34e7347e2d75daaa87e93d
RobertLita <lita.robert@yahoo.com>
2023-05-23 23:14:41 +0300

    cors enabled

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.api_v1.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
